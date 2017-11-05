894f6edc1b05e2d91d366a5dcd158795a9898dfc
Rosiej <mateusz.rosiejka@gmail.com>
2017-11-05 15:48:36 +0100

    Added safety conditions

--- src/pl/sdacademy/java7krkr/Main.java
+++ src/pl/sdacademy/java7krkr/Main.java
@@ -6,12 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("wpisz ciąg");
+        System.out.println("wpisz ciąg camelCase");
         String camel = scanner.nextLine();
         int words =0;
+        if(camel.isEmpty()||Character.isUpperCase(camel.charAt(0))){
+            System.out.println("to nie jest camelCase");
+            return;
+        }
         for (int i = 0; i <camel.length() ; i++) {
             if(i==0){
                 words++;
+            }
+            if(Character.isDigit(camel.charAt(i))){
+                System.out.println("ciąg nie może zawierać cyfr");
+                return;
             }
             if (Character.isUpperCase(camel.charAt(i))){
                 words+=1;
